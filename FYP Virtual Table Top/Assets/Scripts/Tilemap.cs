c0e284c26e0bd0b2cdae1376da3675670dd24640
Marcus Daly <daly.marcus@gmail.com>
2015-11-08 16:51:28 +0000

    Basic Texture Mapping and Tiles
    Basic texture mapping and tiling as well as a few sample art assets

--- FYP Virtual Table Top/Assets/Scripts/Tilemap.cs
+++ FYP Virtual Table Top/Assets/Scripts/Tilemap.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[ExecuteInEditMode]
 [RequireComponent(typeof(MeshFilter))]
 [RequireComponent(typeof(MeshRenderer))]
 [RequireComponent(typeof(MeshCollider))]
@@ -29,8 +30,9 @@ public class Tilemap : MonoBehaviour {
 
         //create mesh data
         Vector3[] vertices = new Vector3[numverts];
-        int[] triangles = new int[numtris *2];
+        int[] triangles = new int[numtris * 3];
         Vector3[] normals = new Vector3[numverts];
+        Vector2[] uv = new Vector2[numverts];
 
         int x, z;
         for (z =0; z< size_z; z++)
@@ -39,6 +41,7 @@ public class Tilemap : MonoBehaviour {
             {
                 vertices[z * vsize_x + x] = new Vector3(x*tilesize, z*tilesize, 0);
                 normals[z * vsize_x + x] = Vector3.up;
+                uv[z * vsize_x + x] = new Vector2((float)x / vsize_x, (float)z / vsize_z);
 
             }
         }
@@ -48,14 +51,14 @@ public class Tilemap : MonoBehaviour {
             for (x = 0; x < size_x; x++)
             {
                 int squareindex = z * size_x + x;
-                int triindex = squareindex * 6;
-                triangles[triindex + 0] = z * vsize_x + x + 0;
-                triangles[triindex + 1] = z * vsize_x + x + vsize_x + 1;
-                triangles[triindex + 2] = z * vsize_x + x + vsize_x + 0;
+                int triOffset = squareindex * 6;
+                triangles[triOffset + 0] = z * vsize_x + x + 0;
+                triangles[triOffset + 1] = z * vsize_x + x + vsize_x + 0;
+                triangles[triOffset + 2] = z * vsize_x + x + vsize_x + 1;
 
-                triangles[triindex + 3] = z * vsize_x + x + 0;
-                triangles[triindex + 4] = z * vsize_x + x + 1;
-                triangles[triindex + 5] = z * vsize_x + x + vsize_x + 1;
+                triangles[triOffset + 3] = z * vsize_x + x + 0;
+                triangles[triOffset + 4] = z * vsize_x + x + vsize_x + 1;
+                triangles[triOffset + 5] = z * vsize_x + x + 1;
 
             }
         }
@@ -68,6 +71,7 @@ public class Tilemap : MonoBehaviour {
         mesh.vertices = vertices;
         mesh.triangles = triangles;
         mesh.normals = normals;
+        mesh.uv = uv;
 
 
         //assign mesh to filterer/renderer/collider
@@ -76,6 +80,7 @@ public class Tilemap : MonoBehaviour {
         MeshCollider mc = GetComponent<MeshCollider>();
 
         mf.mesh = mesh;
+        mc.sharedMesh = mesh;
 
     }
 }
