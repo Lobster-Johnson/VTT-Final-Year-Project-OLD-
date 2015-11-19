3ca5c867da649ceb879c663e19c3f0ccb7a46d5b
Marcus Daly <daly.marcus@gmail.com>
2015-11-19 17:48:17 +0000

    Continued Work on Touch Controls

--- FYP Virtual Table Top/Assets/screencontrols.cs
+++ FYP Virtual Table Top/Assets/screencontrols.cs
@@ -1,17 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class SimpleCameraControl : MonoBehaviour {
-    public KeyCode forward;
-    public KeyCode left;
-    public KeyCode right;
-    public KeyCode backward;
-
-    public float speed = 10;
-
-
-
-
+public class TouchControls : MonoBehaviour {
     public float movesensitivityX = 1.0f;
     public float movesensitivityY = 1.0f;
 
@@ -25,26 +15,9 @@ public class SimpleCameraControl : MonoBehaviour {
         Screen.orientation = ScreenOrientation.LandscapeLeft;
     }
 
-    // Update is called once per frame
-    void Update () {
-        //wasd controls for debug
-        if (Input.GetKey(forward))
-        {
-            transform.Translate(0, speed * Time.deltaTime, 0);
-        }
-        if (Input.GetKey(left))
-        {
-            transform.Translate(-speed * Time.deltaTime, 0, 0);
-        }
-        if (Input.GetKey(right))
-        {
-            transform.Translate(speed * Time.deltaTime, 0, 0);
-        }
-        if (Input.GetKey(backward))
-        {
-            transform.Translate(0, -speed * Time.deltaTime, 0);
-        }
-
+	
+	// Update is called once per frame
+	void Update () {
         //touchscreen camera controls
 
 
@@ -65,18 +38,12 @@ public class SimpleCameraControl : MonoBehaviour {
             if (Input.GetTouch(0).phase == TouchPhase.Moved)
             {
                 Debug.Log("Touch Moved");
-                Vector2 delta = Input.GetTouch(0).deltaPosition;
 
-                float positionX = (delta.x * movesensitivityX * Time.deltaTime) * -1;
-                float positionY = (delta.y * movesensitivityY * Time.deltaTime) * -1;
-
-                transform.position += new Vector3(positionX, positionY, -1);
             }
             if (Input.GetTouch(0).phase == TouchPhase.Ended)
             {
                 Debug.Log("Touch Ended");
             }
         }
-
     }
 }
